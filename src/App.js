ef1a7440522e1a053fd05edd5d53ce5d2ef62212
jarvisd86 <86847293+jarvisd86@users.noreply.github.com>
2021-07-19 11:20:43 -0700

    new-Updated-branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 // import logo from './logo.svg';
 import "./sass/main.css";
-
-function App() {
+// import Contact from "./components/Contact";
+import React from 'react'
+const  App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Catch Some Vibes</h1>
         <nav>
           <ul>
-            <li>home</li>
-            <li>Info</li>
+             <a href="./components/LandingPage.js">home</a>
+
+           {/* <li><a href="./components/Contact.js">Contact</a></li>  */}
+          {/* <Contact /> */}
+           <a href="./components/Contact.js">Contact</a>
           </ul>
         </nav>
       </header>
