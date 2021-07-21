e952d451b8a7bcf1c2c92bd701c2a47f60cc456f
jarvisd86 <86847293+jarvisd86@users.noreply.github.com>
2021-07-21 10:37:18 -0700

    component update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-
 import "./sass/main.css";
 
-import React from 'react';
-import{ Link, Route, BrowserRouter } from "react-router-dom";
+import React from "react";
+import { Link, Route, BrowserRouter } from "react-router-dom";
 import Contact from "./components/Contact";
 import LandingPage from "./components/LandingPage";
 
+import Img0 from "./marketing/image.jpg";
+import Img1 from "./marketing/image2.jpg";
+import Img2 from "./marketing/image3.jpg";
 
-const  App = () => {
+const App = () => {
   return (
     <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to={"/landingPage"}>landing</Link>
+              </li>
+              <li>
+                <Link to={"/contact"}>Contact</Link>
 
-        
-        <nav>
-          <ul>
-     
-            <li><Link to={"/landingPage"}>landing</Link>
-          </li> 
-          <li><Link to={"/contact"}>Contact</Link>
-          </li>
-        
-          </ul>
-         
-        </nav>
-      </header>
-        <Route exact path='/landingPage' component={LandingPage} />
-        <Route exact path='/contact' component={Contact} />
-      
-    </div>
+              </li>
+              <li>
+                <Link to={"/products"}>products</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Route exact path="/landingPage" component={LandingPage} />
+        <Route exact path="/contact" component={Contact} />
+
+        <Route exact path="/products">
+          <h1> marketing services </h1>
+
+          <div className="items">
+            <img src={Img0} alt="#" />
+          </div>
+          <div className="items">
+            <img src={Img1} alt="#" />
+          </div>
+          <div className="items">
+            <img src={Img2} alt="#" />
+          </div>
+          <h2>Catch Some Vibes</h2>
+        </Route>
+      </div>
     </BrowserRouter>
-
   );
-}
+};
 
 export default App;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,21 +1,11 @@
 import React from 'react'
- 
-import Img0 from './marketing/image.jpg';
-import Img1 from './marketing/image2.jpg';
-import Img2 from './marketing/image3.jpg';
-
 
 
 
 const LandingPage = () => {
     return (
         <div className='landing-div'>
-            <h1> marketing services </h1>
-            
-            <div className='items'><img src={Img0} alt='#'/></div>
-            <div className='items'><img src={Img1} alt='#'/></div>
-            <div className='items'><img src={Img2} alt='#'/></div>
-             <h2>Catch Some Vibes</h2>
+         
         </div>
     )
 }
